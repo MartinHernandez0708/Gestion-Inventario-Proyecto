5bf2637fd778c83a69ecea34712829f6a00573f6
MartinHernandez0708 <mamemimomu1234martin@gmail.com>
2025-07-18 14:00:36 -0500

    Create main.py
    nuevo archivo principal

---
+++ main.py
@@ -0,0 +1,6 @@
+import modulo = mo
+
+def menu():
+    print("Bienvenido usuario, elija una opción")
+    eleccion = int(input("1_ Agregar producto\n2_ Eliminar producto\n3_ Modificar producto\n4_ Mostrar productos actuales\n5_ Salir"))
+    return eleccion
