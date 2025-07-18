7679f37c7a4bbe1e32fc368885c3cfd8c5ecd079
alexander456lt <159347075+alexander456lt@users.noreply.github.com>
2025-07-18 13:19:14 -0500

    Add files via upload
    Podemos trabajar atreves de esta base

---
+++ trabajo.py
@@ -0,0 +1,14 @@
+#Proyecto gestion de inventario
+
+class Producto:
+    def __init__(self, nombre, cantidad, precio):
+        self.nombre = nombre
+        self.cantidad = cantidad
+        self.precio = precio
+
+    def __str__(self):
+        return f"Producto: {self.nombre}, Cantidad: {self.cantidad}, Precio: {self.precio}"
+    
+class Gestor_Inventario:
+    def __init__(self):
+        self.productos = {}
