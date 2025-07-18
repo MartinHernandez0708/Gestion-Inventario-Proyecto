a07598a4bf1d4fef56c98c35dcd21b8c35c78147
MartinHernandez0708 <mamemimomu1234martin@gmail.com>
2025-07-18 13:59:39 -0500

    Update and rename trabajo.py to modulo.py
    Aqui estaran las clases y las funciones y las importaremos a un archivo main que controlara todo

--- modulo.py
+++ modulo.py
@@ -39,9 +39,3 @@ class User:
         self.inventario = []
     def __str__(self):
         return f"Nombre: {self.nombre}\nContraseña: {self.contraseña}\nInventario: {self.inventario}"
-    def agregar_producto(self, nombre_producto, cantidad_producto, precio_producto=0):
-        for producto in self.inventario:
-            if nombre_producto == producto.nombre:
-                producto.cantidad += cantidad_producto
-                return
-        self.inventario.append(Producto(nombre_producto, cantidad_producto, precio_producto))
